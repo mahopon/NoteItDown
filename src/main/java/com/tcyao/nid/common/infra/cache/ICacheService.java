0ea894f077436fffa68dc54354641e8db374c8af
Chong Yao <56928030+mahopon@users.noreply.github.com>
2026-09-07 01:12:13 +0800

    chore:comment out cache code

--- src/main/java/com/tcyao/nid/common/infra/cache/ICacheService.java
+++ src/main/java/com/tcyao/nid/common/infra/cache/ICacheService.java
@@ -1,12 +1,12 @@
-package com.tcyao.nid.common.infra.cache;
-
-import java.time.Duration;
-
-
-public interface ICacheService {
-    void put(String key, Object value, Duration ttl);
-
-    <T> T get(String key, Class<T> type);
-
-    void delete(String key);
-}
+//package com.tcyao.nid.common.infra.cache;
+//
+//import java.time.Duration;
+//
+//
+//public interface ICacheService {
+//    void put(String key, Object value, Duration ttl);
+//
+//    <T> T get(String key, Class<T> type);
+//
+//    void delete(String key);
+//}
